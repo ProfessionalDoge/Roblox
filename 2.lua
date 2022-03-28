22cd327367ec7ffb4bbfcf63606752444249a53f
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-03-28 10:55:47 +0800

    Delete 2.lua

--- 2.lua
+++
@@ -1,7 +0,0 @@
-local PlaceId = game.PlaceId
-
-if PlaceId == 6071175527 then
-  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Hello", Text = "Welcome to my place"})
-  else
-  
-  end
